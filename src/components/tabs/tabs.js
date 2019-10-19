aaf9f884da0af5d682dbe94443b3e9d434e756cc
yc111 <yinchuan_111@163.com>
2019-10-19 22:43:15 +0800

    complete tabs

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -3,17 +3,17 @@ const uuidv4 = require('uuid');
 
 class Tabs {
     constructor(options) {
-        if(!options || typeof options !== 'object'){
+        if (!options || typeof options !== 'object') {
             throw new Error('new Tabs must pass options whitch is an object type');
         }
         this.options = options;
 
-        if(!this.options.el || this.options.el.slice(0,1) !== '#') {
+        if (!this.options.el || this.options.el.slice(0, 1) !== '#') {
             throw new Error('need options.el which is a string type and begins with "#"');
         }
         this.el = this.options.el;
 
-        if(!this.options.list || !Array.isArray(this.options.list)) {
+        if (!this.options.list || !Array.isArray(this.options.list)) {
             throw new Error('need options.list whitch is an array type');
         }
         this.list = this.options.list;
@@ -21,7 +21,7 @@ class Tabs {
         let selected = this.options.selected || 0;
         if (!isNaN(parseInt(selected)) && selected >= 0 && selected < this.list.length) {
             this.selected = selected;
-        }else {
+        } else {
             this.selected = 0;
         }
 
@@ -32,7 +32,7 @@ class Tabs {
 
         let len = this.list.length;
         this.panelIds = [];
-        for(i = 0; i < len; i++) {
+        for (i = 0; i < len; i++) {
             this.panelIds.push(uuidv4());
         }
         this.activePanelId = this.panelIds[this.selected];
@@ -54,14 +54,18 @@ class Tabs {
         $(this.el).html(html);
 
         $(`#${this.id} .uco-tabs-nav`).click((e) => {
-            if($(e.target).hasClass('uco-tabs-tab')) {
+            if ($(e.target).hasClass('uco-tabs-tab')) {
                 $(`#${this.id} .uco-tabs-tab`).removeClass('active');
                 $(e.target).addClass('active');
 
                 this.selected = $(`#${this.id} .uco-tabs-tab`).index(e.target);
                 this.activePanelId = this.panelIds[this.selected];
+
                 $(`#${this.id} .uco-tabs-panel`).removeClass('active');
-                $(`#${this.activePanelId}`).addClass('active');
+                $(`#${this.activePanelId}`).addClass('active')
+                $(`#${this.id} .uco-tabs-content`).css({
+                    transform: `translateX(-${this.selected}00%) translateZ(0px)`
+                });
 
                 this.events['onchange'].forEach(fn => {
                     fn({
@@ -83,7 +87,7 @@ class Tabs {
     }
 
     onchange(cb) {
-        if(cb) {
+        if (cb) {
             this.events['onchange'].push(cb);
         }
     }
